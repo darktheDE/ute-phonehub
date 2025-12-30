23bb5546eb76e2de25d3557759ef3947e0f32f98
Duy Nguyễn <fansjaki@gmail.com>
2025-12-30 12:58:10 +0700

    fix(hooks): add missing useCategories and useBrands exports

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -8,5 +8,7 @@ export { useProducts, useFeaturedProducts } from "./useProducts";
 export { useOrders } from "./useOrders";
 export { useUsers } from "./useUsers";
 export { useDashboard } from "./useDashboard";
+export { useCategories } from "./useCategories";
+export { useBrands } from "./useBrands";
 export { usePromotions } from "./usePromotions";
 export { useAvailablePromotions } from "./useAvailablePromotions";
